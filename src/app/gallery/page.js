6595bdeb8aacea6ba1185b639220f890bc942b28
Ashwani Yadav <yadav8800766165@gmail.com>
2026-05-23 21:56:57 +0530

    new filter updat

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import FloatingButtons from "@/components/CallButton";
-import decorations from "../../../data/decorations";
+import decorations from "@/data/decorations.json";
 import { getCategory } from "@/lib/getCategory";
 
 export default function GalleryPage() {
